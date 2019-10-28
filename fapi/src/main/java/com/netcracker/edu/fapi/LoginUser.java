297ef31aa8801eb838694ed068d5bdc81d9ff017
Valeria-Sem <lerasemenenya@gmail.com>
2019-10-28 23:39:34 +0300

    create directory models and Java class - Users

--- fapi/src/main/java/com/netcracker/edu/fapi/LoginUser.java
+++
@@ -1,7 +0,0 @@
-package com.netcracker.edu.fapi;
-
-public class LoginUser {
-
-    private String username;
-    private String password;
-}
---
+++ fapi/src/main/java/models/Users.java
@@ -0,0 +1,65 @@
+package models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Users {
+
+    private long id;
+    private String login;
+    private String password;
+    private String role;
+
+    public Users() {
+    }
+
+    public Users(long id, String login, String password, String role) {
+        this.id = id;
+        this.login = login;
+        this.password = password;
+        this.role = role;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
+}
+
